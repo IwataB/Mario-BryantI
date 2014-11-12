2fa359f384d6464c2aa156dfe38def2172f4b6c2
Bryant Iwata <pbomber07@gmail.com>
2014-11-12 09:15:08 -0800

    End of 11/12/14

--- public_html/Mario Files/js/resources.js
+++ public_html/Mario Files/js/resources.js
@@ -4,7 +4,7 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
+        {name: "background-tiles", type:"image", src: "data/img/backgroun-tiles.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -15,7 +15,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+        {name: "BryantI-Level01", type: "tmx", src: "data/map/BryantI-Level01.tmx"},
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
