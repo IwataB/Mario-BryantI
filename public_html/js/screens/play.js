9dcc768f23508dfadb880b05363f34c3099b47f6
Bryant Iwata <pbomber07@gmail.com>
2014-11-13 09:17:48 -0800

    End of 11/13/14

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -6,7 +6,7 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
 
-                me.levelDirector.levelLoad("BryantI-Level01");
+                me.levelDirector.loadLevel("BryantI-Level01");
 
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
