cf3295e1586199db7f5ce16dfd3e8c34c4bd8616
Bryant Iwata <pbomber07@gmail.com>
2014-11-14 09:18:36 -0800

    End of 11/14/14

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,1 +1,24 @@
 // TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function (x, y, settings){
+        this._super(me.Entity)
+        settings.image = "Mario";
+        settings.spritewidth = "128";
+        settings.spriteheight = "128";
+        settings.width = 128;
+        settings.height = 128;
+        this._super(me.Entity, "init", [x, y, settings]);
+        
+        this.body.setVelocity(5, 0);
+    },
+    update: function (delta){
+        if(me.input.isKeyPressed("right")){
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        }
+        else{
+            this.body.vel.x = 0;
+        }
+        this.body.update(delta);
+        return true;
+    }
+});
