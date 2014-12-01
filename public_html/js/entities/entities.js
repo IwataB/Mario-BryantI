d8cbdded74f2e07b7763907b5bf844dc3af3c4bd
Bryant Iwata <pbomber07@gmail.com>
2014-12-01 10:45:34 -0800

    End of 12/1/14

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,4 +1,4 @@
-// TODO
+//adds size to the entity
 game.PlayerEntity = me.Entity.extend({
     init: function (x, y, settings){
         this._super(me.Entity, 'init', [x, y,{
@@ -12,7 +12,7 @@ game.PlayerEntity = me.Entity.extend({
             return (new me.Rect(0, 0, 128, 128)).toPolygon();
         }
         }]);
-        
+        //adds animations
         this.renderable.addAnimation("idle", [3]);
         this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
         
@@ -20,11 +20,11 @@ game.PlayerEntity = me.Entity.extend({
         
         this.body.setVelocity(5, 20);
     },
+    //update function
+    update: function(delta){
+    me.collision.check(this, true, this.collideHandler.bind(this), true);
     
     this.body.update(delta);
-    me.collision.check(this, true, this.collideHandler.bind(this))
-    
-    update: function (delta){
         console.log("update");
         if(me.input.isKeyPressed("right")){
             console.log("right");
@@ -33,6 +33,9 @@ game.PlayerEntity = me.Entity.extend({
         else{
             this.body.vel.x = 0;
         }
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true)
+        
         if(this.body.vel.x !==0){
             if(!this.renderable.isCurrentAnimation("smallWalk")){
             this.renderable.setCurrentAnimation("smallWalk");
@@ -40,12 +43,16 @@ game.PlayerEntity = me.Entity.extend({
         }else{    
             this.renderable.setCurrentAnimation("idle");
             }
-        this.body.update(delta);
+       
         this._super(me.Entity, "update", [delta]);
         return true;
+        },
+        
+        collideHandler: function(response){
+            
         }
     });
-    
+    //checks collision with "levelTrigger"
     game.levelTrigger = me.Entity.extend({
         init: function (x, y, settings){
             this._super(me.Entity, 'init', [x, y, settings]);
@@ -55,5 +62,5 @@ game.PlayerEntity = me.Entity.extend({
          onColllision: function(){ 
              this.body.setCollisionMask(me.collision.types.NO_OBJECT);
              me.levelDirector(this.level);
-        },
+        }
     });
